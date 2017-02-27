7e0fad56d51a999b7a5d1fecff58459a9450ad35
RedEis91 <imitationkrabs91@gmail.com>
2017-02-26 22:04:31 -0500

    Fully Functional, Baby

--- controller.js
+++ controller.js
@@ -1,25 +1,21 @@
+var app = angular.module("twedditModule");
 
 
-app.controller('myController', function ($scope, $http) {
-
-            $scope.posts = "{}";
-
-            return {
-                getData: function () {
+app.controller('twedditController', function ($scope, $http) {
+                function getData () {
                     $http({
                         url: "https://www.reddit.com/r/awww/.json",
                         method: 'GET'
                     })
                         .then(
-                        function successfulCallback(response) {
-                            $scope.posts = response.data.data.children;
-                        }, 
+                        function missionSuccess(response) {
+                          $scope.pulledData = response;
+                          console.log(response);
+                          $scope.posts = $scope.pulledData.data.data.children;
+                        },
                         function (error) {
                             console.log(error);
-                        }); 
-                } 
-                
-            }
-            
-});
-
+                        });
+                }
+                getData();
+            });
